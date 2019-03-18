209350629adaa6a2dc98b09a5bc09552fe21dd09
Kelly Sciandra <kellysciandradev@icloud.com>
2019-03-18 19:34:30 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,8 @@
 def square_array(array)
   new_array = []
-  counter = 0
     array.each do |num|
-    variable = num * num
-  counter += 1 
-  new_array << variable
-end
+      variable = num * num
+    new_array << variable
+  end
 return new_array 
 end
